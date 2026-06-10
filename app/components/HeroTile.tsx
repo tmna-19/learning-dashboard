cea6e6bd3c7a9755248260dfd3d11f5039b0d7a9
tmna-19 <tamannakanwaria19@gmail.com>
2026-06-10 16:40:24 +0530

    y

--- app/components/HeroTile.tsx
+++ app/components/HeroTile.tsx
@@ -1,23 +1,18 @@
 "use client";
-
 import { motion } from "framer-motion";
-
 export default function HeroTile() {
   return (
     <motion.div
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
-      className="bg-blue-500 text-white p-6 rounded-xl"
-    >
+      className="bg-blue-500 text-white p-6 rounded-xl">
       <h1 className="text-3xl font-bold">
         Welcome Back Tamanna 👋
       </h1>
-
       <p className="mt-2">
         Continue your learning journey
       </p>
     </motion.div>
-    
   );
 }
